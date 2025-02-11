363835cb94a2d4a4415dbe3dbc632e4912af3c87
idohaver7 <idohaver7@gmail.com>
2025-02-11 20:00:05 +0200

    add profile picture to the server

--- Server/server/routes/user.js
+++ Server/server/routes/user.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 const userController = require('../controllers/user');
 const auth = require('../middleware/auth');
 
-router.post('/', userController.createUser)
-router.get('/', auth.verifyToken, userController.getUser)
+// Configure storage for multer
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './public/profile/'); // Adjust the path as necessary
+    },
+    filename: function(req, file, cb) {
+            cb(null, req.body.email + path.extname(file.originalname)); // Use email as filename
+        }
+    
+});
+
+const upload = multer({ storage: storage });
+
+router.post('/', upload.single('profilePicture'), userController.createUser);
+router.get('/', auth.verifyToken, userController.getUser);
 
 module.exports = router;
+
